08bedf100f554a8c52d98ab43f2594b29fad193f
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-02-24 21:18:13 +0000

    created the game

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,77 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 //create your first component
-export function Home() {
+
+const Cell = props => (
+	<div className="Cell" onClick={() => props.clicked(props.cellNum)}>
+		{props.val}
+	</div>
+);
+
+export const Board = props => {
+	const initData = {
+		cells: Array.apply(null, { length: 9 }).map(x => ""),
+		symbl: "X"
+	};
+	const [cells, setCells] = React.useState(initData.cells);
+	const [symbl, setSymbl] = React.useState(initData.symbl);
+
+	const tttStates = [
+		"111000000",
+		"000111000",
+		"000000111",
+		"100100100",
+		"010010010",
+		"001001001",
+		"100010001",
+		"001010100"
+	];
+
+	const moveBitmap = symbl => cells.map(x => (x === symbl ? 1 : 0)).join("");
+
+	const checkWinner = symbl => {
+		for (let i of tttStates) {
+			if (
+				(parseInt(moveBitmap(symbl), 2) & parseInt(i, 2)) ===
+				parseInt(i, 2)
+			) {
+				alert(`${symbl} won! New Game?`);
+				newGame();
+			}
+		}
+	};
+
+	const clicked = x => {
+		const newCellData = Array(...cells);
+		if (!newCellData[x]) {
+			newCellData[x] = symbl;
+			setCells(newCellData);
+			setSymbl(symbl === "X" ? "O" : "X");
+		}
+	};
+
+	const newGame = () => {
+		setCells(initData.cells);
+		setSymbl(initData.symbl);
+	};
+
+	const vCells = [];
+	for (let i = 0; i < 9; i++) {
+		vCells.push(<Cell val={cells[i]} cellNum={i} clicked={clicked} />);
+	}
+
+	React.useEffect(() => checkWinner(symbl === "X" ? "O" : "X"));
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Game">
+			<div className="Board">{vCells}</div>
+			<p>Next move: {symbl}</p>
+			<button onClick={newGame}>New Game</button>
 		</div>
 	);
-}
+};
+Cell.propTypes = {
+	clicked: PropTypes.func,
+	cellNum: PropTypes.string,
+	val: PropTypes.string
+};
